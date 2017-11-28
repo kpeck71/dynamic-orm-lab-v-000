50e5747e8374c1a298730fa6ec1dcac08a9c7df0
Kristen Peck <kpeck71@gmail.com>
2017-11-28 01:47:13 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,7 @@ class InteractiveRecord
 
   def self.find_by(attributes)
     key = attributes.keys[0].to_s
-    value = attributes[value].to_s
+    value = attributes[key].to_s
     sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}'"
     DB[:conn].execute(sql)
   end
